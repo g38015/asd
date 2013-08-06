7f68ef6f062210d170799792bffe13bb42af1a9d
appstrategy <eprleads@gmail.com>
2013-08-06 16:52:27 -0700

    function saving to local storage working, refactored submit for jquery, object setup with jquery

--- week2/js/main.js
+++ week2/js/main.js
@@ -8,4 +8,43 @@ $('#home').on('pageinit', function(){
 
 $('#additem').on('pageinit', function(){
 	//any other code needed for addItem page goes here
+    
+    var storeData = function(data, key){
+    
+    // if there is no key means brand new lead and need a new key
+    if(!key) {
+    
+        var id             = Math.floor(Math.random()*10000001);
+        
+    }else{
+        // set the id to the existing key we are editing
+            id             = key;
+        }
+        // Get all form field values and store in object
+        // Object properties contain array form label and input value
+        //getSelectedRadio();
+        var lead           = {};
+            lead.name      = ["Name:", $("#name").val()];
+            lead.phone     = ["Phone:", $("#phone").val()];
+            lead.email     = ["Email:", $("#email").val()];
+            lead.date      = ["Date:", $("#mydate").val()];            
+            
+        // Save data to local storage Use stringify to convert object to a string
+        localStorage.setItem(id, JSON.stringify(lead));
+        alert("Lead Has Been Saved!");
+    
+    }; 
+	
+
+	$('#submit').click(function () {
+	storeData();
+	});
+	
+	
 });
+
+$('#show').on('pageinit', function(){
+	//any other code needed for addItem page goes here
+});
+
+
