5e2de5880961d5e7b33b0486944818d69ee99ef3
appstrategy <eprleads@gmail.com>
2013-08-06 13:04:34 -0700

    project structure for week 2

---
+++ week2/js/main.js
@@ -0,0 +1,11 @@
+// Peter Hitchcock
+// ASD 1308
+// Week 2
+
+$('#home').on('pageinit', function(){
+	//code needed for home page goes here
+});
+
+$('#additem').on('pageinit', function(){
+	//any other code needed for addItem page goes here
+});
