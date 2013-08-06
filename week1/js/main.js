3acef21196a74de4a023c7508e48e47302dc4055
appstrategy <eprleads@gmail.com>
2013-08-06 12:39:27 -0700

    Initial IA Design Concept utilizing jquery mobile

---
+++ week1/js/main.js
@@ -0,0 +1,11 @@
+// Peter Hitchcock
+// ASD 1308
+// Week 1
+
+$('#home').on('pageinit', function(){
+	//code needed for home page goes here
+});
+
+$('#additem').on('pageinit', function(){
+	//any other code needed for addItem page goes here
+});
