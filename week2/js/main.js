b866df441679194951382a4775db4f6f4fb37a63
appstrategy <eprleads@gmail.com>
2013-08-12 13:25:45 -0700

    updated main.js and index.html, cleaned up files

--- week2/js/main.js
+++ week2/js/main.js
@@ -2,12 +2,9 @@
 // ASD 1308
 // Week 2
 
-
-
 $('#home').on('pageinit', function(){
     
     $('#jsontest').on('click', function () {
-        
         
         //$('#leads').empty();
         $.ajax({
@@ -17,25 +14,18 @@ $('#home').on('pageinit', function(){
             success: function(response){
                 console.log(response.lead.length);
                 for (var i=0, j=response.lead.length; i<j; i++) {
-                    console.log(i, j);
-                    var l = response.lead[i];
-                    $('<li>' + '<a href="#blogpost">'+ l.name + '</a>'+'</li>').appendTo('#leads');
-
                     
+                    // Add li to lead page
+                    var l = response.lead[i];
+                    $('<li>' + '<a href="#showLead">'+ l.name + '</a>'+'</li>').appendTo('#leads');
+
+                    // Add output to show page
                     var output='';
                     output += '<h2>'+ l.name +'</h2>';
                     output += '<h3>'+ l.email +'</h3>';
                     output += l.phone; 
-                    $('#mypost').html(output);
+                    $('#myleads').html(output);
                         
-                   
-                    /*
-                    for (var n in response) {
-                      var id = Math.floor(Math.random()*10000001);
-                        localStorage.setItem(id, JSON.stringify(response[n]));
-
-                    }
-                    */
                 };
                 
                 $('#leads').listview('refresh');
@@ -47,7 +37,6 @@ $('#home').on('pageinit', function(){
 
     $('#xml').on('click', function () {
         
-        
         //$('#leads').empty();
         $.ajax({
             url: "xhr/data.xml",
@@ -57,25 +46,23 @@ $('#home').on('pageinit', function(){
                 console.log("xml data loaded");
                 console.log(response);
                 $(response).find('leads lead').each(function (){
-                    // Parse for li
+                   
+                   // Add li to lead page
                     var name = $(this).find('name').text();
-                    $('<li>' + '<a href="#blogpost">'+ name + '</a>'+'</li>' 
+                    $('<li>' + '<a href="#showLead">'+ name + '</a>'+'</li>' 
                     ).appendTo('#leads');
-                    // Parse for show page
+                    
+                    // Add output to show page
                     var eamil = $(this).find('email').text();
                     var output='';
                     output += '<h2>'+ name +'</h2>';
                     output += '<p>'+ email + '</p>';
-                    $('#mypost').html(output);
+                    $('#myleads').html(output);
                     
-
-
                 })
+
                 $('#leads').listview('refresh');
 
-            // Extract relevant data from XML
-            //var xml_node = $('speakers',data);
-                //console.log( xml_node.find('speaker >name').text() );
             },
             
             error: function() {
@@ -86,14 +73,13 @@ $('#home').on('pageinit', function(){
         });
         
     });
+
     var getData = function(){
     if (localStorage.length === 0) {
-        alert("You Have No Leads, Defalt Lead Info Added");
-        
+        alert("You Have No Leads Please Add One");
         } else {
            
             //Write local data from local storage to browser
-          
             for (var i = 0, len=localStorage.length; i<len; i++) {
                 var l = localStorage.length;
                 var key = localStorage.key(i);
@@ -105,33 +91,29 @@ $('#home').on('pageinit', function(){
                     console.log(value);
                     console.log(obj);
 
+                    // Add li to lead page
+                    $('<li>' + '<a href="#showLead">'+ obj.name[1] + '</a>'+'</li>').appendTo('#leads');
 
-                    $('<li>' + '<a href="#blogpost">'+ obj.name[1] + '</a>'+'</li>').appendTo('#leads');
-
-                    
+                    // Add output to show page
                     var output='';
                     output += '<h2>'+ obj.name[1] +'</h2>';
                     output += '<h3>'+ obj.phone[1] +'</h3>';
                     output += obj.email[1]; 
-                    $('#mypost').html(output);
+                    $('#myleads').html(output);
                     
-                //loadImage(obj.bedrooms[1], newsubLi);
-                
-                //createLeadLinks(localStorage.key(i), linkLi);
+              
             } 
+             //window.location.reload();
             $('#leads').listview('refresh');
-
         }   
         
     };
 
     $('#local').on('click', getData);
 
-
 });
 
 $('#additem').on('pageinit', function(){
-    //any other code needed for addItem page goes here
     
     var storeData = function(data, key){
     
@@ -141,12 +123,12 @@ $('#additem').on('pageinit', function(){
         var id             = Math.floor(Math.random()*10000001);
         
     }else{
+
         // set the id to the existing key we are editing
             id             = key;
-        }
+    }
         // Get all form field values and store in object
         // Object properties contain array form label and input value
-        //getSelectedRadio();
         var lead           = {};
             lead.name      = ["Name:", $("#name").val()];
             lead.phone     = ["Phone:", $("#phone").val()];
@@ -158,7 +140,6 @@ $('#additem').on('pageinit', function(){
         alert("Lead Has Been Saved!");
         window.location.reload();
         return false;
-    
     };
 
     $('#submit').on('click', storeData);
@@ -166,7 +147,7 @@ $('#additem').on('pageinit', function(){
 });
 
 // Clear Function Global
-var clearLocal = function(){
+    var clearLocal = function(){
         if (localStorage.length === 0) {
          alert("There Are No Leads to Delete");
         } else {
@@ -180,36 +161,4 @@ var clearLocal = function(){
     $('#clear').on('click', clearLocal);
 
 
-
-/*
-
-        $.ajax({
-            url: "xhr/data.json",
-            type: "GET",
-            dataType: "json",
-            success: function(response){
-                console.log(response.lead.length);
-                for (var i=0, j=response.lead.length; i<j; i++) {
-                    
-
-                    var l = response.lead[i];
-                    console.log(l.name);
-                    var output='';
-                    output += '<h2>'+ l.name +'</h2>';
-                    output += l.bio; 
-                    $('#mypost').html(output);
-                   
-                    
-
-                 
-                   
-
-                };
-                
-                
-            }
-
-        });
-
-*/
 
