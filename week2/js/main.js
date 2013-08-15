305cfe4e3fd0e32806bfad1e55dd7d3ee6a605d6
appstrategy <eprleads@gmail.com>
2013-08-15 09:31:43 -0700

    completed json and xml files, initial edit and delete single record functions

--- week2/js/main.js
+++ week2/js/main.js
@@ -15,7 +15,7 @@ $('#additem').on('pageinit', function(){
         }
     });
     
-    // Stores Data to Local Storage
+    // Stores Data to Local Storage WORKING
     var storeData = function(data, key){
     
     // if there is no key means brand new lead and need a new key
@@ -47,7 +47,7 @@ $('#additem').on('pageinit', function(){
 
 });
 
-// Clear Function
+// Clear All Function WORKING
     var clearLocal = function(){
         if (localStorage.length === 0) {
          alert("There Are No Leads to Delete");
@@ -59,10 +59,38 @@ $('#additem').on('pageinit', function(){
         }
     };
 
-    $('#clear').on('click', clearLocal);
+    $('#clearlocal').on('click', clearLocal);
         
-// Delete Single Lead
-    function deleteSingleLead() {
+// Create Edit and Delete Links for Each Stored Lead when Displayed NOT WORKING
+     function createLeadLinks(key, linkLi) {
+         // Add Edit
+         var editLead = document.createElement("a");
+         editLead.setAttribute("id", "editLead");
+         editLead.href = "#";
+         editLead.key = key;
+         var editText = "Edit";
+         editLead.addEventListener("click", editSingleLead);
+         editLead.innerHTML = editText;
+         linkLi.appendChild(editLead);
+         
+         // Add LineBreak
+         var breakLine = document.createElement("br");
+         linkLi.appendChild(breakLine);
+         
+         // Add Delete
+         var deleteLead = document.createElement('a');
+         deleteLead.setAttribute("id", "deleteLead");
+         deleteLead.href = "#";
+         deleteLead.key = key;
+         var deleteText = "Delete";
+         deleteLead.addEventListener("click", deleteSingleLead);
+         deleteLead.innerHTML = deleteText;
+         linkLi.appendChild(deleteLead);
+         
+     }
+     
+     // Delete Single NOT WORKING
+     function deleteSingleLead() {
          var ask = confirm("Really? Are You Sure?");
          if (ask) {
              localStorage.removeItem(this.key);
@@ -70,7 +98,31 @@ $('#additem').on('pageinit', function(){
          }else{
              alert("Your Lead was not Deleted");
          }
-    }
+     }
+     $('#deleteLead').on('click', deleteSingleLead);
 
-    $('#deleteLead').on('click', deleteSingleLead)
-
+     
+     // Edit Single NOT WORKING
+     function editSingleLead() {
+         // Grab Data from Local Storage
+         var value = localStorage.getItem(this.key);
+         var lead = JSON.parse(value);
+        
+    
+         // Populate Form Fields
+         $("#name").value = lead.name[1];
+         $("#phone").value = lead.phone[1];
+         $("#email").value = lead.email[1];
+         $("#date").value = lead.date[1];
+         
+     
+         // Remove Listener from Input
+         save.removeEventListener("click", storeData);
+         // Change Submit to Say Edit Button
+         $("#submit").value = "Edit Lead";
+         var editSubmitButton = $("#submit");
+         // Save the Key Value Established as a Property of the editSubmit event
+         editSubmitButton.addEventListener("click", validation);
+         editSubmitButton.key = this.key;
+         
+     }
